3754041b9d134717f686ff1f8028b1cfb6d5e555
江 <ivan555ivan555@gmail.com>
2018-03-21 01:11:12 +0800

    add file

---
+++ src/components/Test.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="test">
+		我是測試組件中的內容
+		{{msg}}
+	</div>
+</template>
+
+<script>
+export default{
+	data(){
+		return{
+			msg:"我是data屬性中的數據"
+		}
+	}
+}
+</script>
+
+<style>
+.test{
+	color: red;
+}
+</style>
