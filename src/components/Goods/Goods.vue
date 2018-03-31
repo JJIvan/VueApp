d4521ab1418bc03d3e497d2586bfe644f01ec602
Unknown <ivan555ivan555@gmail.com>
2018-03-31 13:05:19 +0800

    goods 左側側邊攔

--- src/components/Goods/Goods.vue
+++ src/components/Goods/Goods.vue
@@ -2,7 +2,28 @@
 	<div class="goods">
 		<!-- 分類 -->
 		<div class="menu-wrapper">
-			
+			<ul>
+
+
+				<!-- 專場 -->
+				<li class="menu-item">
+					<p class="text">
+						<img :src="container.tag_icon" v-if="container.tag_icon" class="icon">
+						{{container.tag_name}}
+					</p>
+				</li>
+
+
+				<!--  -->
+				<li class="menu-item" v-for="item in goods">
+					<p class="text">
+						<img :src="item.icon" v-if="item.icon" class="icon">
+						{{item.name}}
+					</p>
+				</li>
+
+
+			</ul>
 		</div>
 		<!-- 商品列表 -->
 		<div class="foods-wrapper"></div>
@@ -10,8 +31,30 @@
 </template>
 <script>
 	export default{
-		
+		data(){
+			return {
+				container:{},
+				goods:[]
+			}
+		},
+		created(){   //ajax
+
+		    var that =this;
+
+		    this.$axios.get('/api/goods')
+		        .then(function (response) {
+		          var dataSource = response.data;
+		          if(dataSource.code == 0){
+					that.container=dataSource.data.container_operation_source;
+					that.goods=dataSource.data.food_spu_tags;   
+		          }
+		        })
+		        .catch(function (error) {
+		          console.log(error);
+		        });
+ 		 }
 	}
+	
 </script>
 <style>
 	@import url("Goods.css");
